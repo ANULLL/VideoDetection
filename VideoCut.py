a54092316f1ba6673a53ef0f63d7767b38a57a31
fligignat <49873542+fligignat@users.noreply.github.com>
2019-04-24 10:19:40 +0300

    universal folder path + loop for all files

--- VideoCut.py
+++ VideoCut.py
@@ -1,14 +1,36 @@
 import cv2
 import matplotlib.pyplot as plt
-vidcap = cv2.VideoCapture('video.mp4')
-success, image = vidcap.read()
-count = 0
-success = True
-while success:
-    vidcap.set(cv2.CAP_PROP_POS_MSEC, count*1000)
+import os
+try:
+    from pathlib import Path
+except ImportError:             # Python 2
+    from pathlib2 import Path
+# path to video files
+directory=Path.cwd()
+files = os.listdir(directory)
+videos = filter(lambda x: x.endswith('.mp4'), files)
+num_file=0
+for file in videos:
+    print(file)
+    vidcap = cv2.VideoCapture(file)
     success, image = vidcap.read()
-    print('Read a new frame: ', success)
-    cv2.imwrite('/Users/maksimpolakov/PycharmProjects/untitled/venv/frames/frame%d.jpg' % count, image)  # save frame as JPEG file
-    count += 1
-    #plt.imshow(image)
-    #plt.show()
+    count = 0
+    success = True
+    # определим имя директории, которую создаём
+    path = 'frames{id}'.format(id=num_file)
+
+    try:
+        os.mkdir(path)
+    except OSError:
+        print("Создать директорию %s не удалось" % path)
+    else:
+        print("Успешно создана директория %s " % path)
+    while success:
+        vidcap.set(cv2.CAP_PROP_POS_MSEC, count*1000)
+        success, image = vidcap.read()
+        print('Read a new frame: ', success)
+        cv2.imwrite('frames{id}/frame%d.jpg'.format(id=num_file) % count, image)  # save frame as JPEG file
+        count += 1
+            #plt.imshow(image)
+            #plt.show()
+    num_file+=1
--- VideoNew.py
+++ VideoNew.py
@@ -1,26 +1,30 @@
 import cv2
 import matplotlib.pyplot as plt
 import numpy as np
+import os
 try:
     from pathlib import Path
 except ImportError:             # Python 2
     from pathlib2 import Path
-p = Path('/Users/maksimpolakov/PycharmProjects/untitled/venv/frames')
-
+directory=Path.cwd()
+files = os.listdir(directory)
+pictures = filter(lambda x: x.startswith('frames'), files)
+count=1
+for file in pictures:
+    print(file)
 # читаем кадры
-imgs = [cv2.imread(str(f)) for f in p.glob('*.jpg')]
-#plt.imshow(imgs[10])
-#plt.show()
+    p=(str(directory)+'/'+file)
+    p=Path(p)
+    imgs = [cv2.imread(str(f)) for f in p.glob('*.jpg')]
 # создаем видео
-width,height, layers = imgs[0].shape
-#print(height)
-#print(width)
-fps = 1
-capSize = (height,width) # this is the size of my source video
-fourcc = cv2.VideoWriter_fourcc(*'m', 'p', '4', 'v') # note the lower case
-vout = cv2.VideoWriter()
-success = vout.open('output.avi',fourcc,fps,capSize,True)
-_=[vout.write(i) for i in imgs]
-cv2.destroyAllWindows()
-vout.release()
-
+    width,height, layers = imgs[0].shape
+    fps = 1
+    capSize = (height,width) # this is the size of my source video
+    fourcc = cv2.VideoWriter_fourcc(*'m', 'p', '4', 'v') # note the lower case
+    vout = cv2.VideoWriter()
+    success = vout.open('output{id}.avi'.format(id=count),fourcc,fps,capSize,True)
+    _=[vout.write(i) for i in imgs]
+    print("Видео успешно создано")
+    cv2.destroyAllWindows()
+    vout.release()
+    count-=1
