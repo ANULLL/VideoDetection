938e00f035e91e917a15ff1a9a40b4c0bc2bf850
fligignat <49873542+fligignat@users.noreply.github.com>
2019-04-24 11:17:55 +0300

    new: parsing time
    Именовать в папке со скриптом видео в формате 
    05.04.2019_09.25.17.mp4
    05.04.2019 - дата
    09.25.17- время первого кадра

--- VideoCut.py
+++ VideoCut.py
@@ -1,10 +1,20 @@
 import cv2
 import matplotlib.pyplot as plt
 import os
+import datetime as dt
 try:
     from pathlib import Path
 except ImportError:             # Python 2
     from pathlib2 import Path
+def parser(f):
+    i=f.find('_')+1
+    j=len(f)
+    data=f[i:j:1]
+    hour=data[0:2:1]
+    minute=data[3:5:1]
+    second=data[6:8:1]
+    d=dt.time(int(hour),int(minute),int(second))
+    return d
 # path to video files
 directory=Path.cwd()
 files = os.listdir(directory)
@@ -12,9 +22,10 @@ videos = filter(lambda x: x.endswith('.mp4'), files)
 num_file=0
 for file in videos:
     print(file)
+    num_file=parser(file)
     vidcap = cv2.VideoCapture(file)
     success, image = vidcap.read()
-    count = 0
+    count = num_file.second
     success = True
     # определим имя директории, которую создаём
     path = 'frames{id}'.format(id=num_file)
@@ -33,4 +44,5 @@ for file in videos:
         count += 1
             #plt.imshow(image)
             #plt.show()
-    num_file+=1
+
+    #num_file+=1
--- VideoNew.py
+++ VideoNew.py
@@ -2,19 +2,32 @@ import cv2
 import matplotlib.pyplot as plt
 import numpy as np
 import os
+import datetime as dt
 try:
     from pathlib import Path
 except ImportError:             # Python 2
     from pathlib2 import Path
+def parser(f):
+    i=f.find('s')+1
+    j=len(f)
+    data=f[i:j:1]
+    hour=data[0:2:1]
+    minute=data[3:5:1]
+    second=data[6:8:1]
+    d=dt.time(int(hour),int(minute),int(second))
+    return d
 directory=Path.cwd()
 files = os.listdir(directory)
 pictures = filter(lambda x: x.startswith('frames'), files)
-count=1
+#count=1
 for file in pictures:
-    print(file)
+    #print(file)
+    count=parser(file)
+    #print(count)
 # читаем кадры
     p=(str(directory)+'/'+file)
     p=Path(p)
+    print(p)
     imgs = [cv2.imread(str(f)) for f in p.glob('*.jpg')]
 # создаем видео
     width,height, layers = imgs[0].shape
@@ -22,9 +35,9 @@ for file in pictures:
     capSize = (height,width) # this is the size of my source video
     fourcc = cv2.VideoWriter_fourcc(*'m', 'p', '4', 'v') # note the lower case
     vout = cv2.VideoWriter()
-    success = vout.open('output{id}.avi'.format(id=count),fourcc,fps,capSize,True)
+    success = vout.open('out{id}.avi'.format(id='_'+str(count.hour)+'.'+ str(count.minute)+'.'+str(count.second)),fourcc,fps,capSize,True)
     _=[vout.write(i) for i in imgs]
     print("Видео успешно создано")
     cv2.destroyAllWindows()
     vout.release()
-    count-=1
+    #count+=1
