06279f65d62712ed2d989966a1e00ee08d19c3f5
fligignat <49873542+fligignat@users.noreply.github.com>
2019-04-24 16:25:58 +0300

    file names=placce_ID+data+time
    Именовать видео в формате:
    наименование места_день.месяц.год_часы.минуты.секунды
    (часы.минуты.секунды - первого кадра)
    Пример:
    placeid_05.04.2019_09.25.17.mp4

--- VideoCut.py
+++ VideoCut.py
@@ -6,7 +6,24 @@ try:
     from pathlib import Path
 except ImportError:             # Python 2
     from pathlib2 import Path
-def parser(f):
+def correct_time(time): # добавляем секунду каждому кадру и корректируем остальное время
+    h =time.hour
+    m=time.minute
+    s=time.second
+    s+=1
+    if s>=60:
+        m+= 1
+        s= s % 60
+    if m>=60:
+        h+= 1
+        m= m % 60
+    if h>=24:
+        h=0
+        m=0
+        s=0
+    time=time.replace(hour=h,minute=m,second=s)
+    return time
+def parser_time(f):
     i=f.find('_')+1
     j=len(f)
     data=f[i:j:1]
@@ -15,20 +32,32 @@ def parser(f):
     second=data[6:8:1]
     d=dt.time(int(hour),int(minute),int(second))
     return d
+def parser_date(f):
+    i = f.find('_') + 1
+    j = len(f)
+    idplace=f[0:i:1]
+    data = f[i:j:1]
+    day = data[0:2:1]
+    month = data[3:5:1]
+    year = data[6:10:1]
+    d = dt.date(int(year),int(month),int(day))
+    return idplace,d,data
 # path to video files
 directory=Path.cwd()
 files = os.listdir(directory)
 videos = filter(lambda x: x.endswith('.mp4'), files)
 num_file=0
 for file in videos:
+    i=0
     print(file)
-    num_file=parser(file)
+    idplace,date,num_file=parser_date(file)
+    print(idplace+'_'+str(date))
+    num_dir=num_file=parser_time(num_file)
     vidcap = cv2.VideoCapture(file)
     success, image = vidcap.read()
-    count = num_file.second
     success = True
     # определим имя директории, которую создаём
-    path = 'frames{id}'.format(id=num_file)
+    path = 'frames{id}'.format(id='.'+idplace+str(date)+'_'+str(num_dir))
 
     try:
         os.mkdir(path)
@@ -37,12 +66,16 @@ for file in videos:
     else:
         print("Успешно создана директория %s " % path)
     while success:
-        vidcap.set(cv2.CAP_PROP_POS_MSEC, count*1000)
+        vidcap.set(cv2.CAP_PROP_POS_MSEC, i*1000)
         success, image = vidcap.read()
         print('Read a new frame: ', success)
-        cv2.imwrite('frames{id}/frame%d.jpg'.format(id=num_file) % count, image)  # save frame as JPEG file
-        count += 1
+          # save frame as JPEG file
+        cv2.imwrite('frames{id}/frame{time}.jpg'.format(id='.'+idplace+str(date)+'_'+str(num_dir),time='_'+idplace+str(date)+'_'+str(num_file)), image)
+
+        #count += 1
             #plt.imshow(image)
             #plt.show()
+        i+=1
+        num_file = correct_time(num_file)
 
     #num_file+=1
--- VideoNew.py
+++ VideoNew.py
@@ -7,8 +7,8 @@ try:
     from pathlib import Path
 except ImportError:             # Python 2
     from pathlib2 import Path
-def parser(f):
-    i=f.find('s')+1
+def parser_time(f):
+    i=f.find('_')+1
     j=len(f)
     data=f[i:j:1]
     hour=data[0:2:1]
@@ -16,13 +16,29 @@ def parser(f):
     second=data[6:8:1]
     d=dt.time(int(hour),int(minute),int(second))
     return d
+def parser_date(f):
+    i = f.find('_') + 1
+    j = len(f)
+    idplace=f[0:i:1]
+    data = f[i:j:1]
+    year=data[0:4:1]
+    month=data[5:7:1]
+    day=data[8:10:1]
+    #day = data[0:2:1]
+    #month = data[3:5:1]
+    #year = data[6:10:1]
+    d = dt.date(int(year),int(month),int(day))
+    return idplace,d,data
 directory=Path.cwd()
 files = os.listdir(directory)
 pictures = filter(lambda x: x.startswith('frames'), files)
 #count=1
 for file in pictures:
     #print(file)
-    count=parser(file)
+    #count=parser(file)
+    idplace, date, num_file = parser_date(file)
+    print(idplace + '_' + str(date))
+    num_dir = num_file = parser_time(num_file)
     #print(count)
 # читаем кадры
     p=(str(directory)+'/'+file)
@@ -35,9 +51,9 @@ for file in pictures:
     capSize = (height,width) # this is the size of my source video
     fourcc = cv2.VideoWriter_fourcc(*'m', 'p', '4', 'v') # note the lower case
     vout = cv2.VideoWriter()
-    success = vout.open('out{id}.avi'.format(id='_'+str(count.hour)+'.'+ str(count.minute)+'.'+str(count.second)),fourcc,fps,capSize,True)
+    success = vout.open('{id}.avi'.format(id=idplace+str(date)+'_'+str(num_dir)),fourcc,fps,capSize,True)
     _=[vout.write(i) for i in imgs]
-    print("Видео успешно создано")
+    print("Видео файл успешно создан")
     cv2.destroyAllWindows()
     vout.release()
     #count+=1
